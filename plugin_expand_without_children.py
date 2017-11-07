c629fe922f648ced39ee563100cd7c3dfcf8cbb3
Renan Pallin <renanpallin@gmail.com>
2017-11-07 13:53:55 -0200

    Putting cursor rigth before />

--- plugin_expand_without_children.py
+++ plugin_expand_without_children.py
@@ -4,12 +4,15 @@ import sublime_plugin
 
 class ExpandWithoutChildren(sublime_plugin.TextCommand):
 	def run(self, edit):
-		position = self.view.sel()[0]
-		region = self.view.word(position)
+		cursor_position = self.view.sel()[0]
+		region = self.view.word(cursor_position)
 		word = self.view.substr(region)
 		expanded_word = "<%s />" % word
 		self.view.replace(edit, region, expanded_word);
 
-		# @todo: Put cursor rigth before '/>'
-		# self.view.sel().clear()
-		# self.view.sel().add()
+		new_position = self.view.sel()[0]
+		new_position.a -= 2 
+		new_position.b -= 2 
+		self.view.sel().clear();
+		self.view.sel().add(new_position);
+
